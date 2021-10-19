e71862630828f502fc0ef2b38bcee255eb369ba8
Sothanforax <sothanforax@gmail.com>
2021-10-19 14:06:23 -0600

    Removed the messed up blitting and replaced it with a renderer to draw simple shapes

--- sdl2d/omicron.cpp
+++ sdl2d/omicron.cpp
@@ -21,21 +21,23 @@ int main(){
 	SDL_GetWindowSize(win, &wx, &wy);
 	std::cout << "SDL_GetWindowSize returns " << wx << "x" << wy << std::endl;
 	SDL_Surface *win_surface = SDL_GetWindowSurface(win);
-	SDL_Surface *img_surface = SDL_LoadBMP("cliffft.bmp");
-	SDL_Renderer *mainrenderer;
-	SDL_CreateSoftwareRenderer(win_surface);
-	SDL_SetRenderDrawColor(mainrenderer, 255, 0, 0, 255);
-	SDL_RenderDrawLine(mainrenderer, 0, 0, 640, 480);
-	SDL_Rect bmprect;
-	if(!img_surface){
-		std::cout << SDL_GetError();
+	SDL_Renderer *mainrenderer = SDL_CreateSoftwareRenderer(win_surface);
+	if(!mainrenderer){
+		std::cout << "Failed to create renderer" << std::endl;
+		std::cout << SDL_GetError() << std::endl;
 		return -1;
 	}
-	std::cout << "Bitmap image successfully loaded into surface" << std::endl;
-	bmprect.x = 0;
-	bmprect.y = 0;
-	bmprect.w = 256;
-	bmprect.h = 256;
+	std::cout << "Renderer successfully created" << std::endl;
+	SDL_Rect ssrect1;
+	SDL_Rect ssrect2;
+	ssrect1.w = 100;
+	ssrect1.h = 100;
+	ssrect1.x = 200;
+	ssrect1.y = 200;
+	ssrect2.w = 100;
+	ssrect2.h = 100;
+	ssrect2.x = 100;
+	ssrect2.y = 100;
 	SDL_Event ex;
 	while(keepalive == true){
 		while(SDL_PollEvent(&ex) > 0){
@@ -43,8 +45,14 @@ int main(){
 				case SDL_QUIT:
 					keepalive = false;
 			}
-			SDL_BlitSurface(img_surface, NULL, win_surface, &bmprect);
 			SDL_RenderClear(mainrenderer);
+			SDL_SetRenderDrawColor(mainrenderer, 0, 255, 255, 255);
+			SDL_RenderFillRect(mainrenderer, &ssrect1);
+			SDL_SetRenderDrawColor(mainrenderer, 255, 255, 255, 255);
+			SDL_RenderDrawLine(mainrenderer, 0, 0, 400, 400);
+			SDL_SetRenderDrawColor(mainrenderer, 255, 0, 0, 255);
+			SDL_RenderFillRect(mainrenderer, &ssrect2);
+			SDL_SetRenderDrawColor(mainrenderer, 0, 0, 0, 255);
 			SDL_RenderPresent(mainrenderer);
 			SDL_UpdateWindowSurface(win);
 	}
